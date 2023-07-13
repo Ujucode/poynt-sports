aff818f6fb9bb90276a91e81a1177156d341f7dc
kaushik.bora <herock76@gmail.com>
2023-07-13 19:24:41 +0530

    -added dev dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,15 +3,30 @@ module.exports = {
   env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
+    'plugin:import/errors',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:jsx-a11y/recommended',
     'plugin:react-hooks/recommended',
     'prettier',
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: { version: '18.2' },
+    'import/resolver': {
+      node: { extensions: ['.js', '.jsx'] },
+    },
+  },
+  plugins: ['react', 'import', 'jsx-a11y', 'react-refresh'],
   rules: {
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
