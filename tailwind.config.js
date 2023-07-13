8adfb2ae99ea3cec02bec62194bd03fa064572d1
kaushik.bora <herock76@gmail.com>
2023-07-14 04:18:38 +0530

    -added color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        dark: { 1: '#2D3748' },
+        white: { 0: 'white', 1: '#F7F9FC' },
+        green: { 0: '#00AB55', 1: '#50AD29' },
+        gray: {
+          100: '#f7fafc',
+          // ...
+          900: '#1a202c',
+        },
+      },
+      fontFamily: {
+        serif: ['serif'],
+      },
+      fontSize: {
+        '2xl': '12.5rem',
+      },
+    },
   },
   plugins: [],
 };
