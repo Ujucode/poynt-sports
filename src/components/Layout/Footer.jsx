8b10528c562ebdf807cf81c2ba2a721db1264258
kaushik.bora <herock76@gmail.com>
2023-07-15 02:54:40 +0530

    -added content for debugging

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,8 +1,18 @@
+import classes from "./Layout.module.css";
+
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="bg-blue-950">
+      <div className={`${classes["header-container"]} `}>
+        <h1>Footer</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          reprehenderit fuga labore numquam quasi exercitationem distinctio
+          laboriosam laborum nesciunt nemo, qui eius optio perferendis doloribus
+          soluta aliquid necessitatibus. Ullam, non?
+        </p>
+      </div>
+    </footer>
   );
 };
 
