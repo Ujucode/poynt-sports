45c99aa80d41d49c52c559b4ab36c0e24280f84c
kaushik.bora <herock76@gmail.com>
2023-07-15 02:12:08 +0530

    -added main component

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,5 +1,48 @@
+import classes from "./Layout.module.css";
+
 const Main = () => {
-  return <section className='bg-red-200'>Main</section>;
+  return (
+    <section className={`bg-gray-300 `}>
+      <div className={`${classes["header-container"]}`}>
+        <h1>Main section</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint,
+          deleniti! Nam rerum ad itaque dicta eius, quasi impedit, a mollitia
+          tenetur odit nulla! Non natus sunt numquam at tempora recusandae!
+          Vitae blanditiis doloribus vel quisquam veritatis suscipit quia
+          deleniti sunt eius dolor, assumenda eaque nam quas, velit alias
+          cupiditate id eveniet harum hic a! Sit sed voluptatem officia
+          laudantium amet! Nemo, dignissimos velit expedita voluptate dolorum
+          ipsam aspernatur vero? Consectetur, vitae voluptatem amet quod iure
+          porro cupiditate ducimus sequi quae excepturi hic culpa, rem atque
+          inventore odit eum similique sunt! Molestiae minima sapiente facilis
+          harum perspiciatis molestias amet eaque cupiditate laboriosam? Saepe
+          sequi provident ut commodi aperiam! Ab nesciunt illo ad porro, animi,
+          id, dolorum exercitationem doloribus dolore itaque tempore! Ab, aut
+          nesciunt praesentium magnam aspernatur aperiam? Ducimus debitis
+          praesentium aut ut minus numquam. Odio architecto, id praesentium
+          itaque magnam ut dolore, repudiandae quia expedita nesciunt maiores
+          officia asperiores saepe! Numquam autem perferendis quae, sunt, earum
+          et assumenda ducimus, fugiat consequatur totam eum obcaecati magni
+          veniam? Ullam, aspernatur totam odio libero, nam, ea ipsam quibusdam
+          non eveniet ducimus molestias unde? At corporis amet itaque recusandae
+          error accusantium ducimus labore libero, voluptatum ea ullam debitis.
+          Delectus harum quis dicta quisquam odit inventore molestias recusandae
+          deleniti consectetur voluptas assumenda, iure nemo odio. Autem error
+          explicabo accusantium labore aliquid dolorum harum perspiciatis
+          nesciunt tenetur eligendi alias cum deleniti, ut minima facere
+          deserunt. Ab et voluptatum odit? Facere libero exercitationem voluptas
+          ab. Facilis, reiciendis? Molestias quod ea esse quasi iste, libero
+          enim magnam, debitis repudiandae aspernatur excepturi sed at magni
+          quas sequi, eaque rerum ad deserunt accusantium dolorum. Ipsum ipsam
+          nemo earum repellendus ducimus! Nostrum explicabo amet, at odio
+          repellat magni animi eum sapiente, officia, accusantium quod quisquam
+          iusto ex. Mollitia exercitationem nulla a officiis quod quibusdam,
+          esse sit atque asperiores architecto doloribus iure.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Main;
