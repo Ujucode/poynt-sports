22a10205f3eec1e3f1209327291cb6bb460dddd4
kaushik.bora <herock76@gmail.com>
2023-07-17 01:01:22 +0530

    -new rules, added MUI and tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        transparent: 'transparent',
-        dark: { 1: '#2D3748' },
-        white: { 0: 'white', 1: '#F5F5F5' },
-        green: { 0: '#00AB55', 1: '#50AD29' },
+        transparent: "transparent",
+        green: {
+          50: "#f1f6f2",
+          100: "#daf0e7",
+          200: "#ace6c9",
+          300: "#71cc99",
+          400: "#2ead68",
+          500: "#209341",
+          600: "#1c7d2f",
+          700: "#1a6027",
+          800: "#13421f",
+          900: "#0e2919",
+        },
       },
       fontFamily: {
-        roboto: ['Roboto', 'sans-serif'],
-        rubik: ['Rubik', 'sans-serif'],
+        roboto: ["Roboto", "sans-serif"],
+        rubik: ["Rubik", "sans-serif"],
       },
     },
   },
