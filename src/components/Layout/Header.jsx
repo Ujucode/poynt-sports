71647b9fffb03532ec921c87b190ea59e471f3ef
kaushik.bora <herock76@gmail.com>
2023-07-18 04:01:32 +0530

    -applying from theme

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,14 +1,12 @@
 import classes from "./Layout.module.css";
-import Navbar from "./Navbar";
 import HeroMain from "./HeroMain";
 import HeroFooter from "./HeroFooter";
 
 const Header = () => {
   return (
     <header
-      className={`h-screen ${classes.hero} bg-no-repeat bg-cover bg-fixed flex flex-col justify-between text-white-1`}
+      className={`h-screen ${classes.hero} bg-no-repeat bg-cover bg-fixed flex flex-col justify-between text-gray-50`}
     >
-      <Navbar />
       <HeroMain />
       <HeroFooter />
     </header>
