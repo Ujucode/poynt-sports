5ece3355063c54ee72b45b925124753ba90467b3
kaushik.bora <herock76@gmail.com>
2023-07-18 04:03:22 +0530

    -applying theme

--- src/components/Layout/HeroFooter.jsx
+++ src/components/Layout/HeroFooter.jsx
@@ -4,7 +4,7 @@ const HeroFooter = () => {
   return (
     <section>
       <div
-        className={`${classes["header-container"]} flex justify-evenly text-2xl`}
+        className={`${classes["header-container"]} flex justify-evenly text-2xl text-gray-50`}
       >
         <div className=" w-3/12 h-48 flex justify-center align-middle items-center flex-wrap p-8 text-center border-r-2 border-white">
           <p>Events & Tournaments</p>
