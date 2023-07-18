0b3ef2cc8b694513eba3550dcbfff9986cba4c43
kaushik.bora <herock76@gmail.com>
2023-07-19 00:09:33 +0530

    -color pallete fix

--- src/components/Layout/HeroMain.jsx
+++ src/components/Layout/HeroMain.jsx
@@ -6,10 +6,10 @@ const HeroMain = () => {
     <section>
       <div className={` ${classes["header-container"]} leading-4`}>
         <h1 className="text-9xl">
-          Poynt <span className="text-[#00AB55] ">Sports</span>
+          Poynt <span className="text-green-400 ">Sports</span>
         </h1>
         <p className="text-5xl my-9">Mark Your Game!</p>
-        <Button className="text-4xl rounded-full p-2 mt-6 border-4 border-[#00AB55]  ">
+        <Button className="text-4xl rounded-full p-2 mt-6 border-4 border-green-400  ">
           <a href="/" alt="home-button">
             I&#39;m ready!
           </a>
