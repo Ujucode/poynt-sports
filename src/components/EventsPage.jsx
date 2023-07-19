d06773414319ae88677dcc048f781f940fe5d59f
kaushik.bora <herock76@gmail.com>
2023-07-20 00:23:34 +0530

    -added height

--- src/components/EventsPage.jsx
+++ src/components/EventsPage.jsx
@@ -3,7 +3,7 @@ import Navbar from "./UI/Navbar";
 
 const EventsPage = () => {
   return (
-    <Header className="bg-gradient-to-r from-green-200 to-green-700 h-[80vh]">
+    <Header className="bg-gradient-to-r from-green-200 to-green-700 ">
       <Navbar />
       <div className="grid place-content-center h-full">
         <h1 className="text-green-100 text-6xl">This is events page!</h1>
