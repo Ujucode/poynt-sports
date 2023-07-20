e3bd7d0d79606687ecb1e102057ad5b7d7336ccd
kaushik.bora <herock76@gmail.com>
2023-07-20 14:52:55 +0530

    -created PlayerGallery page

--- src/components/Layout/AthletesGallery.jsx
+++ src/components/Layout/AthletesGallery.jsx
@@ -1,23 +1,22 @@
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Unstable_Grid2";
-import AthleteCard from "./AthleteCard";
+import FlowBiteCard from "../UI/FlowbiteCard";
+import MUIPagination from "../UI/MUIPagination";
+import MUISelect from "../UI/MUISelect";
 
 const AthletesGallery = () => {
   return (
-    <section className="my-20">
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid
-          container
-          spacing={{ xs: 2, md: 3 }}
-          columns={{ xs: 4, sm: 8, md: 12 }}
-        >
-          {Array.from(Array(15)).map((_, index) => (
-            <Grid xs={2} sm={4} md={4} key={index}>
-              <AthleteCard />
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
+    <section className="my-20 ">
+      <MUISelect />
+
+      <div className="grid grid-cols-3 gap-16">
+        {Array.from(Array(15)).map((_, index) => (
+          <div key={index}>
+            <FlowBiteCard />
+          </div>
+        ))}
+      </div>
+      <div className="my-10">
+        <MUIPagination />
+      </div>
     </section>
   );
 };
