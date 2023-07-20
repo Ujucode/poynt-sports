23994b67b1955c70b2d26ad9085606fb9072f5d1
AnanyaBanerjee <ananyabanerjee2002@gmail.com>
2023-07-20 15:46:10 +0530

    -createdforms

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,11 +1,12 @@
 import NewsCard from "../UI/NewsCard";
 import CoachesCard from "./CoachesCard";
 import classes from "./Layout.module.css";
+import LeaveAReply from "./LeaveAReply";
 function Main() {
   return (
     <main className={`${classes["header-container"]}`}>
       <section className="h-screen">
-        <div className="flex-wrap text-center top-0 py-4">
+        <div className="top-0 flex-wrap py-4 text-center">
           <h1 className="mb-4 text-xl font-bold leading-none tracking-tight text-gray-800 md:text-4xl lg:text-5xl dark:text-white-0">
             Athletes
           </h1>
@@ -17,19 +18,19 @@ function Main() {
             strive for greatness in our own lives lorem.
           </p>
 
-          <div className=" flex justify-between w-100 max-w-screen-xl mx-auto p-4 md:py-5">
+          <div className="flex justify-between max-w-screen-xl p-4 mx-auto  w-100 md:py-5">
             <a
               href="/some/valid/link"
-              className="group relative block bg-black"
+              className="relative block bg-black group"
             >
               <img
                 alt="Athlete"
                 src="https://images.unsplash.com/photo-1517466787929-bc90951d0974?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=386&q=80"
-                className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+                className="absolute inset-0 object-cover w-full h-full transition-opacity opacity-75 group-hover:opacity-50"
               />
 
-              <div className="relative text-left border-x p-4 sm:p-6 lg:p-8">
-                <p className="text-sm font-medium uppercase tracking-widest text-pink-600">
+              <div className="relative p-4 text-left border-x sm:p-6 lg:p-8">
+                <p className="text-sm font-medium tracking-widest text-pink-600 uppercase">
                   Football
                 </p>
 
@@ -38,7 +39,7 @@ function Main() {
                 </p>
 
                 <div className="mt-32 sm:mt-48 lg:mt-64">
-                  <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+                  <div className="transition-all transform translate-y-8 opacity-0 group-hover:translate-y-0 group-hover:opacity-100">
                     <p className="text-sm text-white-1">
                       Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                       Omnis perferendis hic asperiores quibusdam quidem
@@ -51,16 +52,16 @@ function Main() {
             </a>
             <a
               href="/some/valid/link"
-              className="group relative block bg-black"
+              className="relative block bg-black group"
             >
               <img
                 alt="Athlete"
                 src="https://images.unsplash.com/photo-1517466787929-bc90951d0974?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=386&q=80"
-                className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+                className="absolute inset-0 object-cover w-full h-full transition-opacity opacity-75 group-hover:opacity-50"
               />
 
-              <div className="relative text-left p-4 sm:p-6 lg:p-8">
-                <p className="text-sm font-medium uppercase tracking-widest text-pink-600">
+              <div className="relative p-4 text-left sm:p-6 lg:p-8">
+                <p className="text-sm font-medium tracking-widest text-pink-600 uppercase">
                   Football
                 </p>
 
@@ -69,7 +70,7 @@ function Main() {
                 </p>
 
                 <div className="mt-32 sm:mt-48 lg:mt-64">
-                  <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+                  <div className="transition-all transform translate-y-8 opacity-0 group-hover:translate-y-0 group-hover:opacity-100">
                     <p className="text-sm text-white-1">
                       Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                       Omnis perferendis hic asperiores quibusdam quidem
@@ -82,16 +83,16 @@ function Main() {
             </a>
             <a
               href="/some/valid/link"
-              className="group relative block bg-black"
+              className="relative block bg-black group"
             >
               <img
                 alt="Athlete"
                 src="https://images.unsplash.com/photo-1517466787929-bc90951d0974?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=386&q=80"
-                className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+                className="absolute inset-0 object-cover w-full h-full transition-opacity opacity-75 group-hover:opacity-50"
               />
 
-              <div className="relative text-left border-x p-4 sm:p-6 lg:p-8">
-                <p className="text-sm font-medium uppercase tracking-widest text-pink-600">
+              <div className="relative p-4 text-left border-x sm:p-6 lg:p-8">
+                <p className="text-sm font-medium tracking-widest text-pink-600 uppercase">
                   Football
                 </p>
                 <p className="text-xl font-bold text-white-0 sm:text-2xl">
@@ -99,7 +100,7 @@ function Main() {
                 </p>
 
                 <div className="mt-32 sm:mt-48 lg:mt-64">
-                  <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+                  <div className="transition-all transform translate-y-8 opacity-0 group-hover:translate-y-0 group-hover:opacity-100">
                     <p className="text-sm text-white-1">
                       Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                       Omnis perferendis hic asperiores quibusdam quidem
@@ -136,7 +137,7 @@ function Main() {
       </section>
 
       <sction>
-        <div className="flex-wrap text-center top-0 py-4">
+        <div className="top-0 flex-wrap py-4 text-center">
           <h1 className="mb-4 text-xl font-bold leading-none tracking-tight text-gray-800 md:text-4xl lg:text-5xl dark:text-white-0">
             Coaches
           </h1>
@@ -147,7 +148,7 @@ function Main() {
             autem vero ipsam?
           </p>
 
-          <div className=" flex justify-between w-100 max-w-screen-xl mx-auto p-4 md:py-5">
+          <div className="flex justify-between max-w-screen-xl p-4 mx-auto  w-100 md:py-5">
             <CoachesCard />
             <CoachesCard />
             <CoachesCard />
@@ -187,6 +188,7 @@ function Main() {
             <NewsCard></NewsCard>
           </div>
         </div>
+        <LeaveAReply />
       </section>
     </main>
   );
