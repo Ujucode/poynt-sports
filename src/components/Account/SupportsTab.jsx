fc5b8129021429d7b4e7d6153d4aef659b6487db
kaushik.bora <herock76@gmail.com>
2023-08-09 21:23:14 +0530

    -renamed

--- src/components/Account/SupportsTab.jsx
+++ src/components/Account/SupportsTab.jsx
@@ -1,5 +1,21 @@
+import AthleteCard from "../UI/Cards/AthleteCard";
+
+const randomNames = [
+  "John Doe",
+  "Jane Smith",
+  "Michael Johnson",
+  "Alice Brown",
+  "Robert Lee",
+];
+
 const SupportsTab = () => {
-  return <p>Supports tab</p>;
+  return (
+    <div className="grid grid-cols-1 ">
+      {randomNames.map((name, index) => (
+        <AthleteCard key={index} name={name} />
+      ))}
+    </div>
+  );
 };
 
 export default SupportsTab;
