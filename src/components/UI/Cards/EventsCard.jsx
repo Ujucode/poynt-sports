69880d94530ad36416bf6d4594e90a31e7860be9
kaushik.bora <herock76@gmail.com>
2023-08-17 03:31:33 +0530

    -changing event-card layout

--- src/components/UI/Cards/EventsCard.jsx
+++ src/components/UI/Cards/EventsCard.jsx
@@ -20,24 +20,7 @@ const EventsCard = ({ title, description, image, venue }) => {
         />
       </Box>
 
-      <Box className="px-2 flex items-center  ">
-        <Typography
-          variant="h4"
-          className=" text-center text-gray-900/80 font-bold p-2 grow"
-          wrap="true"
-        >
-          {title}
-        </Typography>
-        <Box className=" text-gray-900/70 w-min " onClick={handleClick}>
-          {clicked ? (
-            <BookmarkIcon fontSize="large" />
-          ) : (
-            <BookmarkAddedIcon fontSize="large" />
-          )}
-        </Box>
-      </Box>
-
-      <Box className="h-2/6 px-4 flex  gap-5 ">
+      <Box className="h-2/6 px-4 pt-4 flex gap-5 ">
         <Typography
           variant="h5"
           className="w-1/6 text-center text-emerald-600 font-bold p-2  "
@@ -46,12 +29,30 @@ const EventsCard = ({ title, description, image, venue }) => {
           Mar, 27
         </Typography>
         <Box className="grow p-2 ">
-          <Typography variant="h5" className="text-gray-900/80 font-bold">
+          <Typography
+            variant="h5"
+            className=" text-gray-900/80 font-bold uppercase"
+            wrap="true"
+          >
+            {title}
+          </Typography>
+          <Typography variant="h6" className="text-gray-900/60 ">
             Venue
           </Typography>
           <Typography variant="body2" className=" text-gray-900/70   ">
             <LocationOnIcon /> {venue}
           </Typography>
+          <button className="mt-4 text-emerald-600">Learn More</button>
+          <Box
+            className="px-2 inline  text-gray-900/70 w-min cursor-pointer "
+            onClick={handleClick}
+          >
+            {clicked ? (
+              <BookmarkIcon fontSize="large" className="" />
+            ) : (
+              <BookmarkAddedIcon fontSize="large" />
+            )}
+          </Box>
         </Box>
       </Box>
     </Paper>
