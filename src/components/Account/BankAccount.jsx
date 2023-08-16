969a6ca2f639ff301ca6cfb53762fae1e98cba9c
kaushik.bora <herock76@gmail.com>
2023-08-16 15:19:01 +0530

    -fixed form labels

--- src/components/Account/BankAccount.jsx
+++ src/components/Account/BankAccount.jsx
@@ -1,48 +1,72 @@
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import { Button, TextField, Box } from "@mui/material";
+import {
+  TextField,
+  Box,
+  Typography,
+  FormLabel,
+  FormGroup,
+} from "@mui/material";
 
 const BankAccount = () => {
   return (
-    <Box>
-      <Typography variant="h6" gutterBottom>
-        Bank Account Details
-      </Typography>
-      <Grid container spacing={4}>
-        <Grid item xs={7}>
-          <TextField label="UPI ID" size="small" fullWidth required />
-        </Grid>
-        <Grid item xs={7}>
-          <TextField
-            label=" Account Holder Name"
-            size="small"
-            fullWidth
-            required
-          />
-        </Grid>
-        <Grid item xs={7}>
-          <TextField label="Account Number" size="small" fullWidth required />
-        </Grid>
-        <Grid item xs={7}>
-          <TextField label="Bank Name" size="small" fullWidth required />
-        </Grid>
-        <Grid item xs={7}>
-          <TextField label="Branch Name" size="small" fullWidth required />
-        </Grid>
-        <Grid item xs={7}>
-          <TextField label="IFSC Code" size="small" fullWidth required />
-        </Grid>
-        <Grid item xs={12} sm={7}>
-          <Button variant="outlined" size="small">
-            Add New
-          </Button>
-        </Grid>
-        <Grid item xs={12} sm={7}>
-          <Button variant="outlined" size="small">
-            Submit
-          </Button>
-        </Grid>
-      </Grid>
+    <Box className="w-full flex flex-col gap-6 ">
+      <FormLabel>
+        <Typography variant="h4" gutterBottom>
+          Bank Account Details
+        </Typography>
+      </FormLabel>
+
+      <FormGroup>
+        <FormLabel>
+          <Typography variant="h6">UPI ID</Typography>
+        </FormLabel>
+        <TextField label="UPI ID" size="small" />
+      </FormGroup>
+
+      <FormGroup>
+        <FormLabel>
+          <Typography variant="h6">Account Holder Name</Typography>
+        </FormLabel>
+        <TextField label=" Account Holder Name" size="small" />
+      </FormGroup>
+
+      <FormGroup>
+        <FormLabel>
+          <Typography variant="h6">Account Number</Typography>
+        </FormLabel>
+        <TextField label="Account Number" size="small" />
+      </FormGroup>
+
+      <FormGroup>
+        <FormLabel>
+          <Typography variant="h6">Bank Name</Typography>
+        </FormLabel>
+        <TextField label="Bank Name" size="small" />
+      </FormGroup>
+
+      <FormGroup>
+        <FormLabel>
+          <Typography variant="h6">Branch Name</Typography>
+        </FormLabel>
+        <TextField label="Branch Name" size="small" />
+      </FormGroup>
+
+      <FormGroup>
+        <FormLabel>
+          <Typography variant="h6">IFSC Code</Typography>
+        </FormLabel>
+        <TextField label="IFSC Code" size="small" />
+      </FormGroup>
+
+      <button className="p-2 w-[10rem] h-10 mt-2 bg-emerald-500 text-white rounded-md ">
+        Add More
+      </button>
+
+      <button
+        type="submit"
+        className="p-2 w-1/4 h-10 mt-20 bg-emerald-500 text-white rounded-md "
+      >
+        Submit
+      </button>
     </Box>
   );
 };
